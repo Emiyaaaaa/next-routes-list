4e65f69ddebd806abc15950a81fa357e78fa5282
lihaozheng001 <lihaozheng001@realsee.com>
2024-08-12 11:00:31 +0800

    fix

--- src/generate.ts
+++ src/generate.ts
@@ -5,16 +5,16 @@ import { getNextRoutes } from "./getNextRoutes";
 const routes = getNextRoutes();
 
 // write to file
-const template = `${process.argv[1]!.replace(".bin", "").replace(
-	/\/next-routes-list\/.*/g,
+const templatePath = `${process.argv[1]!.replace("/.bin", "").replace(
+	/\/generate-next-routes-list\/*.*/g,
 	"/next-routes-list"
 )}/dist/routes.js`;
 
-const templateString = fs.readFileSync(template, "utf-8");
+const templateString = fs.readFileSync(templatePath, "utf-8");
 
 const newTemplateString = templateString.replace(
 	"const routes = [];",
 	`const routes = ${JSON.stringify(routes)};`
 );
 
-fs.writeFileSync(template, newTemplateString);
+fs.writeFileSync(templatePath, newTemplateString);
